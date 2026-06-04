e18dc2ffb46217a29b4fbe4d059450ca1a73fd2a
Micea Christian <christian-andrei.micea@student.upt.ro>
2026-06-04 22:06:08 +0300

    work on data generator

--- src/data_generator.py
+++ src/data_generator.py
@@ -3,33 +3,41 @@ import random
 from datetime import datetime, timedelta
 import utils
 
+
 def generate_ride(ride_id):
-    #Random messiness
+    ride_id=f"RIDE-{random.randint(1,10000)}"
+
+    bike_id = f"Bike-{random.randint(1, 5000)}"
+
+    user_type = random.choice(["Member", "Casual","Tourist","VIP","Robot","Admin",""])
+    
     start_station = random.choice(list(utils.dict_stations.keys()))
+
     end_station = random.choice(list(utils.dict_stations.keys()))
-
-    distance_km = round(random.uniform(0.5, 20.0), 2)
-
-    duration_minutes = round(distance_km * random.uniform(2, 5), 1)
-
+    
     start_time = datetime.now() - timedelta(
         days=random.randint(0, 365)
     )
-    bike_id = f"Bike-{random.randint(1, 5000)}"
-    end_time = start_time + timedelta(minutes=duration_minutes)
-    user_type = random.choice(["Member", "Casual","Tourist","VIP","Robot","Admin",""])
-    ride_id=f"RIDE-{random.randint(1,10000)}"
+    
+    distance_km = round(random.uniform(0.5, 20.0), 2)
+    
+    duration_minutes = round(distance_km * random.uniform(2, 5), 1)
 
+    end_time = start_time + timedelta(minutes=duration_minutes)
+    
+    status = "Unvalidated"
+    
     current_ride=[
         ride_id,
-        bike_id,      
+        bike_id,
+        user_type,      
         start_station,
         end_station,
         start_time.strftime("%Y-%m-%d %H:%M:%S"),
         end_time.strftime("%Y-%m-%d %H:%M:%S"),
-        distance_km,
         duration_minutes,
-        user_type
+        distance_km,
+        status
     ]
     # Messiness Starter
 
@@ -37,29 +45,50 @@ def generate_ride(ride_id):
         current_ride = introduce_spaces(current_ride)
     if random.random() < utils.PROBABILITY_RECORD_WILL_LACK_BIKE_ID: #only 10% of rides will lack bike_id
         current_ride[1] = ""
+    if random.random() < utils.PROBABILITY_RECORD_WILL_CONTAIN_INVALID_STATIONS: #only 10% of rides will have invalid station names
+        current_ride = create_invalid_station_names(current_ride)
     if random.random() < utils.PROBABILITY_RECORD_WILL_MISSING_STATIONS: #only 10% of rides will have missing Start station
-        current_ride[2] = ""
-    if random.random() < utils.PROBABILITY_RECORD_WILL_MISSING_STATIONS: #only 10% of rides will have missing End station
         current_ride[3] = ""
+    if random.random() < utils.PROBABILITY_RECORD_WILL_MISSING_STATIONS: #only 10% of rides will have missing End station
+        current_ride[4] = ""
 
     return current_ride
 
+
 def introduce_spaces(ride):
     for i, field in enumerate(ride):
-        field=str(field)
+        field = str(field)
+
+        if field == "Unvalidated":
+            continue
+
         for j in range(len(field)):
             if random.random() < utils.PROBABILITY_RECORD_WILL_CONTAIN_SPACES:  # 10% chance to add a space
                 field = field[:j] + ' ' + field[j:]
+
         ride[i] = field
+        
     return ride
 
-def create_invalid_station_names():
-    for i,field in enumerate(ride):
-        field=str(field)
-        for j in range(len(field)):
-            if random.random() < utils.PROBABILITY_RECORD_WILL_CONTAIN_LETTERS:  # 10% chance to add a letter
-                field = field[:j] + random.choice(string.ascii_letters) + field[j:]
-        ride[i] = field
+
+def create_invalid_station_names(ride):
+    start_station = ride[3]
+    end_station = ride[4]
+    
+    new_start_station_len = random.randint(1, len(start_station))
+    new_end_station_len = random.randint(1, len(end_station))
+    
+    start_station = ""
+    for i in range(new_start_station_len):
+        start_station += chr(random.randint(45, 125)) # 45 to avoid messing up the CSV by adding random commas or quotes
+
+    end_station = ""
+    for i in range(new_end_station_len):
+        end_station += chr(random.randint(45, 125)) # 45 to avoid messing up the CSV by adding random commas or quotes
+    
+    ride[3] = start_station
+    ride[4] = end_station
+    
     return ride
 
 
@@ -70,13 +99,14 @@ def create_dataset(filename, num_rows):
         writer.writerow([
             "ride_id",
             "bike_id",
+            "user_type",
             "start_station",
             "end_station",
             "start_time",
             "end_time",
-            "distance_km",
             "duration_minutes",
-            "user_type"
+            "distance_km",
+            "status"
         ])
 
         for ride_id in range(1, num_rows + 1):
